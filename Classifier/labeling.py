5cf7e22c9104019bdff663d6b9d660d2a73179f1
HyelinNAM <lightsalt28@naver.com>
2020-07-03 14:01:59 +0900

    temp_labeling

--- Classifier/labeling.py
+++ Classifier/labeling.py
@@ -17,7 +17,10 @@ import torchvision.transforms as transforms
 
 from resnet_cbam import resnet34_cbam
 
-# Style
+# Gender / Style
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
 
 path = '/home/namhyelin99/googledrive/BOAZ Adv/Data/Face' # 이미지 데이터 path
 # '/home/namhyelin99/adv_data/Face'
@@ -33,6 +36,32 @@ dataloader = DataLoader(image,batch_size=120,shuffle=False)
 
 print(len(dataloader))
 
+# Gender
+class Net(nn.Module):
+  def __init__(self):
+    super(Net,self).__init__()
+    resnet = torchvision.models.resnet18(pretrained=False)
+    self.base = nn.Sequential(*list(resnet.children())[:-1])
+    self.fc = nn.Sequential(
+        nn.Linear(512,64),
+        nn.ReLU(),
+        nn.Dropout(0.5),
+        nn.Linear(64,2)
+    )
+
+  def forward(self,x):
+    x = self.base(x)
+    x = x.view(x.size(0),-1)
+
+    x = self.fc(x)
+
+    return x
+
+Gender = Net()
+Gender = torch.load('drive/My Drive/BOAZ Adv/최종 model/<최종> GenderClassifier - 96.88') # Gender 모델 path
+Gender.to(device)
+
+# Style
 class MyResNet(nn.Module):
   def __init__(self):
     super(MyResNet,self).__init__()
@@ -79,41 +108,67 @@ class MyResNet(nn.Module):
     return x1,x2,x3
 
 Hair = MyResNet()
-model_path = '/home/namhyelin99/adv_data/Model/CBAM classifier - 99 95 90'
-Hair = torch.load(model_path,map_location='cpu')
-#Hair.to(device)
+Hair = torch.load('/home/namhyelin99/adv_data/Model/CBAM classifier - 99 95 90')  # Style 모델 path
+Hair.to(device)
 
 # EVAL #
+Gender.eval()
 Hair.eval()
 
+gender_list = []
 color_list = []
 bang_list = []
 style_list = []
 
-for i, data in enumerate(dataloader,0):
+for i, data in tqdm(enumerate(dataloader,0)):
 
   img, _ = data
 
   img = img.to(device)
   
+  gender = Gender(img)
   color,bang,style = Hair(img)
 
+  _, gender = torch.max(gender,1)
   _, color = torch.max(color,1)
   _, bang = torch.max(bang,1)
   _, style = torch.max(style,1)
 
+  gender_list.append(gender.tolist())
   color_list.append(color.tolist())
   bang_list.append(bang.tolist())
   style_list.append(style.tolist())
 
   label = {'iter':i,'gender':gender_list,'color':color_list,'bang':bang_list,'style':style_list}
 
-  with open('/home/namhyelin99/label_style.json','w') as f:
+  # 코랩에선 중간에 끊길까봐 iter마다 저장하게 한건데 로컬이면 그냥 다 끝나고 한번만 저장해도 될 듯.
+  with open('/home/namhyelin99/label_style.json','w') as f: # json 파일 저장할 루트
     json.dump(label,f,indent=2)
 
-  print(i)
+# json > csv
+with open("drive/My Drive/label_style.json", 'r') as f: #json 파일 저장된 루트
+    label = json.load(f)
 
-torch.cuda.is_available()
+gender = sum(label['gender'],[])
+color = sum(label['color'],[])
+bang = sum(label['bang'],[])
+style = sum(label['style'],[])
+
+gender = pd.DataFrame({'gender':gender})
+color = pd.DataFrame({'color':color})
+bang = pd.DataFrame({'bang':bang})
+style = pd.DataFrame({'style':style})
+
+label = pd.concat([gender,bang,color,style],axis=1)
+label
+
+label.to_csv('drive/My Drive/celebA_label.csv') # csv 저장할 루트
+
+# 데이터 개수 확인
+label['gender'].value_counts()
+label['color'].value_counts()
+label['bang'].value_counts()
+label['style'].value_counts()
 
 
 
